932c14f87fdd17acbfedb4caec97565a7583bff1
119wjw <119wjw@naver.com>
2021-01-14 17:36:26 +0900

    board 생성

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10,10,150,100);
+//board variable
+const boardRowCount = 4;
+const boardColumnCount = 3;
+const boardWidth = 130;
+const boardHeight = 130;
+const boardOffsetTop = (canvas.height - boardHeight * 4) / 2;
+const boardOffsetLeft = (canvas.width - boardWidth * 3) / 2;
+
+var board = [];
+for (var r = 0; r < boardRowCount; r++) {
+  board[r] = [];
+  for (var c = 0; c < boardColumnCount; c++) {
+    var boardX = c * boardWidth + boardOffsetLeft;
+    var boardY = r * boardHeight + boardOffsetTop;
+    board[r][c] = { x: boardX, y: boardY };
+  }
+}
+
+function drawPiece() {
+  var img = new Image();
+  img.addEventListener(
+    "load",
+    function () {
+      ctx.drawImage(img, 0, 0, 100, 100);
+    },
+    false
+  );
+  img.src = "./assets/card(GH).png";
+}
+
+function drawBoard() {
+  for (var r = 0; r < boardRowCount; r++) {
+    for (var c = 0; c < boardColumnCount; c++) {
+      ctx.beginPath();
+      ctx.rect(board[r][c].x, board[r][c].y, boardWidth, boardHeight);
+      if (r == 0) {
+        ctx.fillStyle = "rgba(255,102,102,0.5)";
+      } else if (r == 3) {
+        ctx.fillStyle = "rgba(000,153,000,0.5)";
+      } else {
+        ctx.fillStyle = "rgba(255,204,102,0.5)";
+      }
+      ctx.fill();
+      ctx.strokeStyle = "#000000";
+      ctx.strokeRect(board[r][c].x, board[r][c].y, boardWidth, boardHeight);
+      ctx.closePath();
+    }
+  }
+}
+
+function draw() {
+  drawBoard();
+}
+
+draw();
