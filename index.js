f388b953be36228ba1dd296fd3374e8415dd999a
119wjw <119wjw@naver.com>
2021-01-14 19:12:18 +0900

    dispose card

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 //card variable
-const cardWidth = 100;
-const cardHeight = 100;
+const cardWidth = 110;
+const cardHeight = 110;
 
 //board variable
 const boardRowCount = 4;
@@ -19,18 +19,27 @@ for (var r = 0; r < boardRowCount; r++) {
   for (var c = 0; c < boardColumnCount; c++) {
     var boardX = c * boardWidth + boardOffsetLeft;
     var boardY = r * boardHeight + boardOffsetTop;
-    board[r][c] = { x: boardX, y: boardY };
+    board[r][c] = { x: boardX, y: boardY, card: 0 };
   }
+}
+//init card
+{
+  board[0][0].card = 7;
+  board[0][1].card = 6;
+  board[0][2].card = 8;
+  board[1][1].card = 9;
+  board[2][1].card = 4;
+  board[3][0].card = 3;
+  board[3][1].card = 1;
+  board[3][2].card = 2;
 }
 
 function drawCard(num, x, y) {
   var img = new Image();
   img.onload = function () {
     ctx.drawImage(img, x, y, cardWidth, cardHeight);
-    console.log("asdf");
   };
   img.src = "./assets/" + (num + "") + ".png";
-  console.log(img.src);
 }
 
 function drawBoard() {
@@ -49,13 +58,18 @@ function drawBoard() {
       ctx.strokeStyle = "#000000";
       ctx.strokeRect(board[r][c].x, board[r][c].y, boardWidth, boardHeight);
       ctx.closePath();
+      if (board[r][c].card != 0) {
+        var cardX = board[r][c].x + (boardWidth - cardWidth) / 2;
+        var cardY = board[r][c].y + (boardHeight - cardHeight) / 2;
+        drawCard(board[r][c].card, cardX, cardY);
+      }
     }
   }
 }
 
 function drawInit() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBoard();
-  drawCard(1, 50, 50);
 }
 
 drawInit();
