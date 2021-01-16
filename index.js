5ded6b528dbf502330589d88d1b9cdbdfdda59d9
119wjw <119wjw@naver.com>
2021-01-16 21:56:12 +0900

    refectoring --canvas divided--

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const ctx_board = canvas_board.getContext("2d");
 const ctx_card = canvas_card.getContext("2d");
 
 //////////////////////////////////////////////////////////
-/////////////////////  board func  ///////////////////////
+/////////////////////  draw func  ///////////////////////
 //////////////////////////////////////////////////////////
 const boardRowCount = 4;
 const boardColumnCount = 3;
 const boardSize = 130;
 const boardOffsetTop = (canvas_board.height - boardSize * 4) / 2;
 const boardOffsetLeft = (canvas_board.width - boardSize * 3) / 2;
+const cardSize = 110;
 
 var board = [];
 for (var r = 0; r < boardRowCount; r++) {
@@ -34,44 +35,28 @@ function drawTile(r, c) {
   ctx_board.closePath();
 }
 
-//////////////////////////////////////////////////////////
-/////////////////////  card  func  ///////////////////////
-//////////////////////////////////////////////////////////
-const cardSize = 110;
-
-function Card(num, r, c, stat, team) {
-  this.num = num;
-  this.r = r;
-  this.c = c;
-  this.stat = stat;
-  this.team = team; //0: green, 1: red
+function drawSelected(r, c) {
+  ctx_card.beginPath();
+  ctx_card.rect(board[r][c].x, board[r][c].y, boardSize, boardSize);
+  ctx_card.fillStyle = "rgba(240,120,220,0.5)";
+  ctx_card.fill();
+  ctx_card.closePath();
 }
 
-var cards = [];
-//init card
-{
-  cards[1] = new Card(1, 3, 1, 1, 0);
-  cards[2] = new Card(2, 3, 0, 1, 0);
-  cards[3] = new Card(3, 3, 2, 1, 0);
-  cards[4] = new Card(4, 2, 1, 1, 0);
-  cards[5] = new Card(5, 0, 0, 0, 0);
-  cards[6] = new Card(6, 0, 1, 1, 1);
-  cards[7] = new Card(7, 0, 0, 1, 1);
-  cards[8] = new Card(8, 0, 2, 1, 1);
-  cards[9] = new Card(9, 1, 1, 1, 1);
-  cards[10] = new Card(10, 0, 0, 0, 1);
+function undrawSelected(r, c) {
+  ctx_card.clearRect(board[r][c].x, board[r][c].y, boardSize, boardSize);
+  drawCard(r, c);
 }
 
-function drawCard(card) {
-  if (card.stat == 0) return;
+function drawCard(r, c) {
+  if (board[r][c].card == 0) return;
   var img = new Image();
-  var p = { r: card.r, c: card.c };
-  var cardX = board[p.r][p.c].x + (boardSize - cardSize) / 2;
-  var cardY = board[p.r][p.c].y + (boardSize - cardSize) / 2;
+  var cardX = board[r][c].x + (boardSize - cardSize) / 2;
+  var cardY = board[r][c].y + (boardSize - cardSize) / 2;
   img.onload = function () {
     ctx_card.drawImage(img, cardX, cardY, cardSize, cardSize);
   };
-  img.src = "./assets/" + (card.num + "") + ".png";
+  img.src = "./assets/" + (board[r][c].card + "") + ".png";
 }
 
 //////////////////////////////////////////////////////////
@@ -79,9 +64,22 @@ function drawCard(card) {
 //////////////////////////////////////////////////////////
 
 function init() {
-  for (var r = 0; r < boardRowCount; r++)
-    for (var c = 0; c < boardColumnCount; c++) drawTile(r, c);
-  for (var i = 1; i <= 10; i++) drawCard(cards[i]);
+  {
+    board[0][0].card = 7;
+    board[0][1].card = 6;
+    board[0][2].card = 8;
+    board[1][1].card = 9;
+    board[2][1].card = 4;
+    board[3][0].card = 3;
+    board[3][1].card = 1;
+    board[3][2].card = 2;
+  }
+  for (var r = 0; r < boardRowCount; r++) {
+    for (var c = 0; c < boardColumnCount; c++) {
+      drawTile(r, c);
+      drawCard(r, c);
+    }
+  }
 }
 
 //////////////////////////////////////////////////////////
@@ -89,3 +87,32 @@ function init() {
 //////////////////////////////////////////////////////////
 
 init();
+
+//current status with singleton
+var GAMESTAT = {
+  turn: 0, //0: green, 1: red
+  isSel: 0,
+  selCard: 0,
+  r: -1,
+  c: -1,
+};
+
+canvas_card.addEventListener("click", function (e) {
+  for (var r = 0; r < boardRowCount; r++) {
+    for (var c = 0; c < boardColumnCount; c++) {
+      var sx = board[r][c].x;
+      var ex = board[r][c].x + boardSize;
+      var sy = board[r][c].y;
+      var ey = board[r][c].y + boardSize;
+      if (sx < e.layerX && e.layerX < ex && sy < e.layerY && e.layerY < ey) {
+        if (!GAMESTAT.isSel) {
+          drawSelected(r, c);
+          GAMESTAT.isSel = 1;
+        } else {
+          undrawSelected(r, c);
+          GAMESTAT.isSel = 0;
+        }
+      }
+    }
+  }
+});
