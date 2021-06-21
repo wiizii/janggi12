e7642930318d0e488e9ad0c46fef6792edf95e9a
119wjw <119wjw@naver.com>
2021-06-21 14:43:54 +0900

    add socket room

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { stringify } = require('querystring');
 const app = express();
 const http = require('http').createServer(app);
 const port = 3000;
@@ -33,17 +34,30 @@ app.use((err, req, res, next) => {
 //////////////////////////////////////////
 const io = require('socket.io')(http);
 
+let userRooms = [];
+
 io.on('connection', (socket) => {
 	console.log('a user connected');
 	io.emit('population', io.eio.clientsCount);
-	console.log(io.eio.clientsCount);
+
 	socket.on('message', (msg) => {
 		io.emit('message', msg);
-		console.log('chat message', msg);
+	});
+	socket.on('room-message', (msg) => {
+		io.to(msg.roomId).emit('message', msg.data);
+	});
+
+	socket.on('create', (roomId) => {
+		io.emit('onCreateRoom', roomId);
+	});
+
+	socket.on('join', (roomId) => {
+		socket.join(roomId);
+		io.to(roomId).emit('complete', '입장완료');
 	});
 
 	socket.on('disconnect', () => {
 		io.emit('population', io.eio.clientsCount);
-		console.log('user disconnected');
+		console.log('a user disconnected');
 	});
 });
--- routes/routes.js
+++ routes/routes.js
@@ -13,15 +13,12 @@ router.get('/single', (req, res) => {
 	res.render('single.html');
 });
 
-router.get('/multiplay', (req, res) => {
-	res.render('multiplay.html');
-});
-
 router.get('/rule', (req, res) => {
 	res.render('rule.html');
 });
 
-router.get('/room/*', (req, res) => {
+router.get('/room', (req, res) => {
+	console.log(req.query.id);
 	res.render('multiplay.html');
 });
 
