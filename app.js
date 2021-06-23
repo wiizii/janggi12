9f99a698a275d6d3ae84d30132a8e4ffc5d97a77
119wjw <119wjw@naver.com>
2021-06-23 09:16:36 +0900

    add path

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { stringify } = require('querystring');
+const path = require('path');
 const app = express();
 const http = require('http').createServer(app);
 const port = 3000;
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 http.listen(port, () => {
 	console.log(`server on : http://localhost:${port}/`);
@@ -34,26 +34,48 @@ app.use((err, req, res, next) => {
 //////////////////////////////////////////
 const io = require('socket.io')(http);
 
-let userRooms = [];
+let userRooms = {
+	roomId: 1,
+	makeRoom: function () {
+		userRooms[this.roomId++] = {
+			users: [],
+		};
+		return this.roomId - 1;
+	},
+	joinUser: function (roomId, user) {
+		userRooms[roomId].users.push(user);
+	},
+};
 
 io.on('connection', (socket) => {
 	console.log('a user connected');
 	io.emit('population', io.eio.clientsCount);
 
+	io.emit('room-init', userRooms);
+
 	socket.on('message', (msg) => {
 		io.emit('message', msg);
 	});
+
 	socket.on('room-message', (msg) => {
 		io.to(msg.roomId).emit('message', msg.data);
 	});
 
-	socket.on('create', (roomId) => {
-		io.emit('onCreateRoom', roomId);
+	socket.on('create-room', (id) => {
+		let roomId = userRooms.makeRoom();
+		userRooms.joinUser(roomId, id);
+		io.to(id).emit('redirect', roomId);
+		socket.join(roomId);
+		io.emit('update-room', roomId);
 	});
 
 	socket.on('join', (roomId) => {
 		socket.join(roomId);
-		io.to(roomId).emit('complete', '입장완료');
+		io.to(roomId).emit('join-room', socket.id);
+	});
+
+	socket.on('game-status', (status) => {
+		io.to(userRooms[0]).emit('next-status', status);
 	});
 
 	socket.on('disconnect', () => {
