6de17777fc02312d3fe9a4c80bbfb9db95dcc6a2
119wjw <119wjw@naver.com>
2021-12-20 11:04:06 +0900

    SpringBoot project init
    Close #1

---
+++ src/main/java/btt/janggi12/Janggi12Application.java
@@ -0,0 +1,13 @@
+package btt.janggi12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janggi12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janggi12Application.class, args);
+	}
+
+}
