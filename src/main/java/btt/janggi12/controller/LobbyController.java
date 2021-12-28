5dc82997cea8df6ca2091978a37ef67fc8e9fcbd
119wjw <119wjw@naver.com>
2021-12-28 15:42:49 +0900

    feat : lobby rule 연결
    Related #10

--- src/main/java/btt/janggi12/controller/LobbyController.java
+++ src/main/java/btt/janggi12/controller/LobbyController.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class LobbyController {
 
     @RequestMapping("/")
     public String home(){
         return "home";
     }
 
-    @RequestMapping("/")
+    @RequestMapping("/single")
+    public String single(){
+        return "single";
+    }
+
+    @RequestMapping("/rule")
+    public String rule(){
+        return "rule";
+    }
+
 }
